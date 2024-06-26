18743dded35157d192ba2d6cb836bb555294e048
EC2 Default User <ec2-user@ip-172-31-29-181.eu-west-1.compute.internal>
2024-06-26 13:45:13 +0000

    ses mail send

--- src/components/FormWindow.js
+++ src/components/FormWindow.js
@@ -1,20 +1,38 @@
 import React, { useState } from 'react';
 import '../App.css';
-import ConfirmModal from './confirmModal';
+import ConfirmModal from './ConfirmModal';
+import axios from 'axios';
+var config = require('../config.js');
 
 function FormWindow({ display, setDisplay }) {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
     const [showModal, setShowModal] = useState(false);
+    const [modalMessage, setModalMessage] = useState('');
 
-
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(title, content);
+
+        try {
+            const response = await axios.post(config.api.invokeUrl, {
+                subject: title,
+                body: content
+            });
+
+            if (response.status === 200) {
+                setModalMessage('Email was sent successfully.');
+            } else {
+                setModalMessage('Failed to send email.');
+            }
+        } catch (error) {
+            console.error('Error sending email:', error);
+            setModalMessage('Failed to send email.');
+        }
+
+        setShowModal(true);
         setTitle('');
         setContent('');
-        setShowModal(true);
-    }
+    };
 
     if (!display) {
         return null;
@@ -23,9 +41,9 @@ function FormWindow({ display, setDisplay }) {
     return (
         <div className='formWindow'>
             <button className='exit' onClick={() => setDisplay(false)} />
-            <form className='form' style={{ height: '60%' }}>
+            <form className='form' onSubmit={handleSubmit} style={{ height: '60%' }}>
                 <div className='formHeader'>
-                    <span>Send a mail to cantact withe me</span>
+                    <span>Send a mail to contact with me</span>
                 </div>
                 <div className='emailFormBody'>
                     <div className='formGroup'>
@@ -49,11 +67,11 @@ function FormWindow({ display, setDisplay }) {
                     </div>
 
                     <div style={{ display: 'flex', justifyContent: 'center', height: '100%', alignItems: 'center' }}>
-                        <button className='submitBtn' onClick={handleSubmit}>Submit</button>
+                        <button className='submitBtn' type='submit'>Submit</button>
                     </div>
                 </div>
             </form>
-            <ConfirmModal show={showModal} onClose={setShowModal} message={'Email was sent successfully.'} />
+            <ConfirmModal show={showModal} onClose={() => setShowModal(false)} message={modalMessage} />
         </div>
     );
 }
---
+++ src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    api: {
+        invokeUrl: 'https://9yg15m7fbj.execute-api.eu-west-1.amazonaws.com/prod/',
+    }
+}
